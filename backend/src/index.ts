f4477ccfb8ddfc0ed8c6b68ae4f98b6a55cd2cbc
Dtn284 <dtn284@gmail.com>
2025-04-05 11:52:36 +0700

    build docker

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.get("/", (req, res) => {
+  res.send("This is home route");
+});
+
+
+/* SERVER */
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on part ${port}`);
+});
